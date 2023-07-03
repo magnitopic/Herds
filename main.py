fe5f04e0e161b6e4ee1732243f3ea4a1535cd24b
magnitopic <magnitopic@gmail.com>
2023-07-03 21:36:03 +0200

    food and colours

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import random
-import os
-import time
-import sys
 
 class Player:
     def __init__(self, letter, x, y):
@@ -14,13 +11,16 @@ class Player:
         pass
 
     def __str__(self):
-        return self.letter
+        return f'\033[0;{str(ord(self.letter) - 34)}m{self.letter}\033[0m'
+
 
 class Food:
-    def __init__(self, x, y, amount):
-        self.x = x
-        self.y = y
-        slef.amount = amount
+    def __init__(self, amount):
+        self.amount = amount
+
+    def __str__(self):
+        return str(self.amount)
+
 
 class Board:
     def __init__(self, size):
@@ -31,15 +31,24 @@ class Board:
         if (p.x <= self.size and p.y <= self.size):
             self.board[p.y][p.x] = p
         else:
-            print("Error:\nPlayer position out of map")
+            print("\033[0;31mError:\nPlayer position out of map\033[0m")
+
+    def addFood(self, food_num):
+        for i in range(food_num):
+            food_x = random.randint(0, self.size-1)
+            food_y = random.randint(0, self.size-1)
+            while board.board[food_x][food_y] != '·':
+                food_x = random.randint(0, self.size - 1)
+                food_y = random.randint(0, self.size - 1)
+            self.board[food_y][food_x] = Food(random.randrange(1, 9))
 
     def printBoard(self):
-        for i, element1 in enumerate(self.board):
-            print(*self.board[i])
+        for row in self.board:
+            print(*row)
+        print()
 
 
 def create_players(num, DIM, board):
-    players = []
     i = 0
     while (num):
         jugador_x = random.randint(0, DIM-1)
@@ -47,16 +56,17 @@ def create_players(num, DIM, board):
         while board.board[jugador_x][jugador_y] != '·':
             jugador_x = random.randint(0, DIM-1)
             jugador_y = random.randint(0, DIM-1)
-        players.append(Player(chr(65 + i), jugador_x, jugador_y))
-        board.addPlayer(players[i])
+        board.addPlayer(Player(chr(65 + i), jugador_x, jugador_y))
         num -= 1
         i += 1
-    return players
+
 
 if __name__ == "__main__":
-    size = 5
-    player_num = 2
+    size = 10
+    player_num = 5
     food_num = 4
     board = Board(size)
-    players = create_players(player_num, size, board)
+    create_players(player_num, size, board)
+    board.addFood(food_num)
     board.printBoard()
+
